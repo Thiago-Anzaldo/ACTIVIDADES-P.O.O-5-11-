e7b39289ca44a61d69803b9c37e17b430c497e4b
Thiago Anzaldo <thiagoanzaldo008@gmail.com>
2026-03-26 10:29:54 -0300

    Add files via upload

---
+++ ThiagoAnzaldo-Act3/Punto4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        //4. Se ingresan por teclado tres números, si al menos uno de los valores ingresados es menor a 10, imprimir en pantalla la leyenda "Alguno de los números es menor a diez".
+
+        int num1, num2, num3;
+        string linea;
+
+        Console.Write("Ingrese el primer numero: ");
+        linea = Console.ReadLine()!;
+        num1 = int.Parse(linea);
+
+        Console.Write("Ingrese el segundo numero: ");
+        linea = Console.ReadLine()!;
+        num2 = int.Parse(linea);
+
+        Console.Write("Ingrese el tercer numero: ");
+        linea = Console.ReadLine()!;
+        num3 = int.Parse(linea);
+
+
+        if(num1<10 || num2<10 || num3<10)
+        {
+            Console.WriteLine("Alguno de los números es menor a diez");
+        }
+        Console.ReadKey();
+    }
+}
---
+++ ThiagoAnzaldo-Act3/Punto5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        //5. Escribir un programa que pida ingresar la coordenada de un punto en el plano, es decir dos valores enteros x e y (distintos a cero).Posteriormente imprimir en pantalla en que cuadrante se ubica dicho punto. (1º Cuadrante si x > 0 Y y > 0 , 2º Cuadrante: x < 0 Y y > 0). 
+
+        int x, y;
+        string linea;
+
+        Console.Write("Ingrese la coordenada x (distinta de 0): ");
+        linea = Console.ReadLine()!;
+        x = int.Parse(linea);
+
+        Console.Write("Ingrese la coordenada y (distinta de 0): ");
+        linea = Console.ReadLine()!;
+        y = int.Parse(linea);
+
+        if (x != 0 && y != 0) {
+
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("esta en el 1º Cuadrante");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("esta en el 2º Cuadrante");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("esta en el 3º Cuadrante");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("esta en el 4º Cuadrante");
+            }
+        }
+        else
+        {
+            Console.WriteLine("ingrese un numero distinto de 0");
+        }
+        Console.ReadKey();
+    }
+}
