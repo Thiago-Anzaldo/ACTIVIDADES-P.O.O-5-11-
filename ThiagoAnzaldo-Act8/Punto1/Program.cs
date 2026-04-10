a665d42fcb22bd3ad2b166f04f23f42a85d84bb9
Thiago Anzaldo <thiagoanzaldo008@gmail.com>
2026-04-10 12:02:39 -0300

    Add files via upload

---
+++ ThiagoAnzaldo-Act8/Punto1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    internal class ElementoMenor
+    {
+        /*1. Cargar un vector de n elementos. imprimir el menor y un mensaje si se repite dentro
+        del vector.*/
+        private int[] vector;
+
+        public void Carga() 
+        {
+            int cantidadElementos = 0;
+            string linea;
+            Console.Write("Ingrese el tamaño de vector: ");
+            linea = Console.ReadLine();
+            cantidadElementos=int.Parse(linea);
+
+            vector = new int[cantidadElementos];
+        }
+        public void Menor() 
+        {
+            int menor = 0;
+            string linea;
+            Console.WriteLine("AHORA INGRESE LOS ELEMENTOS DEL VECTOR \n");
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                linea = Console.ReadLine();
+                vector[i] = int.Parse(linea);
+                if (menor == vector[i])
+                {
+                    Console.WriteLine("--SE REPITE--");
+                }
+                if (i == 0)
+                {
+                    menor = vector[0];
+                }
+
+                if (menor > vector[i])
+                {
+                    menor = vector[i];
+                }
+            }
+            Console.WriteLine("Finaliza el programa");
+        }
+        static void Main(string[] args)
+        {
+            ElementoMenor p=new ElementoMenor();
+            p.Carga();
+            p.Menor();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThiagoAnzaldo-Act8/Punto2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    internal class Paises
+    {
+        /*2. Definir un vector donde almacenar los nombres de 5 países. Confeccionar el
+        algoritmo de ordenamiento alfabético.*/
+
+        private string[] paises;
+
+        public void CargaPaises() 
+        {
+            paises = new string[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese un pais: ");
+                paises[i] = Console.ReadLine();
+            }
+        }
+        public void OrdenAlfabetico() 
+        {
+            for (int i = 0; i < 5; i++) 
+            {
+                for(int j = 0; j < 5; j++) 
+                {
+                
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Paises paises = new Paises();
+            paises.CargaPaises();
+        }
+    }
+}
