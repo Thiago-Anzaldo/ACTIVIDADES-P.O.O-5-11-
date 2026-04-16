9cf94a22fee54e0a006ad56775f16ac0790755f2
Thiago-Anzaldo <thiagoanzaldo008@gmail.com>
2026-04-15 21:17:27 -0300

    act8

---
+++ ThiagoAnzaldo-Act8/Punto3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    internal class Ordenamiento
+    {
+        /*3. Cargar un vector de n elementos de tipo entero. Ordenar posteriormente el vector
+        (el orden lo pueden elegir ustedes).*/
+        private int[] vector;
+
+        public void Carga()
+        {
+            string linea;
+            vector = new int[5];
+            for(int i = 0; i < vector.Length; i++)
+            {
+                Console.Write("Ingrese los valores del vector: ");
+                linea = Console.ReadLine();
+                vector[i] = int.Parse(linea);
+            }
+        }
+        public void Ordenar()
+        {
+            int aux = vector[0];
+            for (int k = 0; k < 5; k++)
+            {
+                for (int f = 0; f < 5 - 1 - k; f++)
+                {
+                    if (vector[f] < vector[f + 1])
+                    {
+                        aux = vector[f];
+                        vector[f] = vector[f + 1];
+                        vector[f + 1] = aux;
+                    }
+                }
+            }
+            for (int i = 0; i < vector.Length; i++) 
+            {
+                Console.WriteLine(vector[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Ordenamiento o = new Ordenamiento();
+            o.Carga();
+            o.Ordenar();
+            Console.ReadKey();
+        }
+    }
+}
