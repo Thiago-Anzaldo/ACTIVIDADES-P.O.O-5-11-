a1a301257870945ff5b3b1d07c48e1d4cd5215e1
Thiago-Anzaldo <thiagoanzaldo008@gmail.com>
2026-04-21 23:31:37 -0300

    actividad 9

--- ThiagoAnzaldo-Act9/Punto3/Program.cs
+++ ThiagoAnzaldo-Act9/Punto3/Program.cs
@@ -6,10 +6,74 @@ using System.Threading.Tasks;
 
 namespace Punto3
 {
-    internal class Program
+    internal class Carrera
     {
+        /*3. Se registran los nombres de 5 atletas y sus tiempos (en segundos) en una
+        carrera de 100 metros. El programa debe cargar los datos en dos vectores
+        paralelos, calcular y mostrar el promedio de los tiempos, mostrar el nombre del
+        atleta con mejor y peor tiempo, y mostrar los nombres de quienes superaron el
+        promedio.*/
+
+        private string[] corredores;
+        private int[] tiempos;
+
+        public void CargarDatos()
+        {
+            corredores = new string[5];
+            tiempos = new int[5];
+            for (int i = 0; i < corredores.Length; i++)
+            {
+                Console.Write("Ingrese el nombre del atleta " + (i + 1) + ": ");
+                corredores[i] = Console.ReadLine();
+                Console.Write("Ingrese el tiempo del atleta " + corredores[i] + " en segundos:");
+                tiempos[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public void Promedio()
+        {
+            int sumaTiempos = 0;
+            int promedioTiempo = 0;
+
+            for (int i = 0; i < corredores.Length; i++)
+            {
+                sumaTiempos += tiempos[i];
+            }
+
+            promedioTiempo = sumaTiempos / corredores.Length;
+
+            Console.WriteLine("El promedio de tiempos es: " + promedioTiempo);
+        }
+
+        public void MejorPeorTiempo()
+        {
+            int mejorTiempo = tiempos[0];
+            int peorTiempo = tiempos[0];
+            string nombreMejorAtleta = corredores[0];
+            string nombrePeorAtleta = corredores[0];
+            for (int i = 1; i < corredores.Length; i++)
+            {
+                if (tiempos[i] < mejorTiempo)
+                {
+                    mejorTiempo = tiempos[i];
+                    nombreMejorAtleta = corredores[i];
+                }
+                if (tiempos[i] > peorTiempo)
+                {
+                    peorTiempo = tiempos[i];
+                    nombrePeorAtleta = corredores[i];
+                }
+            }
+            Console.WriteLine("El atleta con el mejor tiempo es: " + nombreMejorAtleta + " con un tiempo de " + mejorTiempo + " segundos.");
+            Console.WriteLine("El atleta con el peor tiempo es: " + nombrePeorAtleta + " con un tiempo de " + peorTiempo + " segundos.");
+        }
         static void Main(string[] args)
         {
+            Carrera c = new Carrera();
+            c.CargarDatos();
+            c.Promedio();
+            c.MejorPeorTiempo();
+            Console.ReadKey();
         }
     }
 }
---
+++ ThiagoAnzaldo-Act9/Punto4/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto4
+{
+    internal class EvaluacionDocentes
+    {
+        /*4. Se realiza una evaluación a 6 docentes por parte de sus alumnos. Se registran
+        sus nombres y puntajes promedio obtenidos (de 1 a 10).
+        Cargar sus datos en vectores paralelos, mostrar docente con calificación más
+        alta y más baja, ordenar los vectores de mayor a menor de acuerdo con la
+        calificación y mostrar en pantalla la cantidad de docentes que aprobaron y
+        desaprobaron (tomando como base que se aprueba con una nota mayor o igual
+        a 6)*/
+
+        private string[] docentes;
+        private int[] calificaciones;
+        public void CargarDatos()
+        {
+            docentes = new string[6];
+            calificaciones = new int[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("nombre del docente: ");
+                docentes[i] = Console.ReadLine();
+                Console.Write("calificación del docente: ");
+                calificaciones[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public void MejorYPeorNota()
+        {
+            int mejorNota = 0;
+            int peorNota = 0;
+
+            for (int i = 1; i < docentes.Length; i++)
+            {
+                if (calificaciones[i] > mejorNota)
+                {
+                    mejorNota = calificaciones[i];
+                }
+                if (calificaciones[i] < peorNota)
+                {
+                    peorNota = calificaciones[i];
+                }
+            }
+
+            Console.WriteLine("La mejor calificación es: " + mejorNota);
+            Console.WriteLine("La peor calificación es: " + peorNota);
+        }
+
+        public void OrdenarDocentes()
+        {
+            int auxNotas = 0;
+            string auxDocentes = "";
+
+            for (int i = 0; i < docentes.Length - 1; i++)
+            {
+                for (int j = 0; j < docentes.Length - 1; j++)
+                {
+                    if (calificaciones[j] < calificaciones[j + 1])
+                    {
+                        auxNotas = calificaciones[j];
+                        calificaciones[j] = calificaciones[j + 1];
+                        calificaciones[j + 1] = auxNotas;
+
+                        auxDocentes = docentes[j];
+                        docentes[j] = docentes[j + 1];
+                        docentes[j + 1] = auxDocentes;
+                    }
+                }
+            }
+            for (int i = 0; i < docentes.Length; i++)
+            {
+                Console.WriteLine(docentes[i] + " Nota sacada: " + calificaciones[i]);
+            }
+        }
+
+        public void CantidadAprobadosDesaprobados()
+        {
+            int aprobados = 0;
+            int desaprobados = 0;
+            for (int i = 0; i < calificaciones.Length; i++)
+            {
+                if (calificaciones[i] >= 6)
+                {
+                    aprobados++;
+                }
+                else
+                {
+                    desaprobados++;
+                }
+            }
+            Console.WriteLine("Cantidad de docentes aprobados: " + aprobados);
+            Console.WriteLine("Cantidad de docentes desaprobados: " + desaprobados);
+        }
+        static void Main(string[] args)
+        {
+            EvaluacionDocentes ed = new EvaluacionDocentes();
+            ed.CargarDatos();
+            ed.MejorYPeorNota();
+            ed.OrdenarDocentes();
+            ed.CantidadAprobadosDesaprobados();
+            Console.ReadKey();
+        }
+    }
+}
