5e48e391f91820d3de14df3c06390bc9232af61a
Thiago Anzaldo <thiagoanzaldo008@gmail.com>
2026-04-30 12:46:02 -0300

    Add files via upload

---
+++ ThiagoAnzaldo-Act10/Punto1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    internal class Matriz1
+    {
+        /*1- Crear una matriz de 2 filas y 5 columnas. Realizar la carga de
+        componentes por columna (es decir primero ingresar toda la primer
+        columna, luego la segunda columna y así sucesivamente)
+        Imprimir luego la matriz.*/
+
+        private int[,] matriz;
+
+        public void Carga() 
+        {
+            matriz = new int[2, 5];
+            string linea;
+
+            for (int i=0; i < matriz.GetLength(1);i++) 
+            {
+                for(int j=0; j < matriz.GetLength(0); j++) 
+                {
+                    Console.Write("Ingrese un componente: ");
+                    linea =Console.ReadLine();
+                    matriz[j,i] = int.Parse(linea);
+                }
+            }
+        }
+        public void Muestra() 
+        {
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write(matriz[i,j] +" ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Matriz1 matriz = new Matriz1();
+            matriz.Carga();
+            matriz.Muestra();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThiagoAnzaldo-Act10/Punto2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    internal class Matriz2
+    {
+        /*2- Crear una matriz de n * m filas (cargar n y m por teclado) Intercambiar la
+        primer fila con la segunda. Imprimir luego la matriz.*/
+        private int[,] matriz;
+
+        public void Carga() 
+        {
+            string linea;
+            int filas = 0;
+            int columna = 0;
+
+            do { 
+            Console.WriteLine("Ingrese la cantidad de filas (Mayor a 1): ");
+            linea = Console.ReadLine();
+            filas = int.Parse(linea);
+            }
+            while (filas < 2) ;
+
+            Console.WriteLine("Ingrese la cantidad de columnas: ");
+            linea=Console.ReadLine();
+            columna = int.Parse(linea);
+
+            matriz = new int[filas, columna];
+        }
+
+        public void Cambio1a2() 
+        { 
+             
+        }
+        static void Main(string[] args)
+        {
+            Matriz2 m2=new Matriz2();
+            m2.Carga();
+        }
+    }
+}
