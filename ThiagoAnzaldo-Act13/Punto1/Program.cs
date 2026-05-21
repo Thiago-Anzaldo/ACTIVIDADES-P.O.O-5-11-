31bc85f83103dc25e9922c7ef8ca670745d84fb4
Thiago Anzaldo <thiagoanzaldo008@gmail.com>
2026-05-21 12:36:59 -0300

    Add files via upload

---
+++ ThiagoAnzaldo-Act13/Punto1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Punto1
+{
+    class Empleado
+    {
+        //1. Confeccionar una clase que represente un empleado.Definir como atributos su
+        //nombre y su sueldo.En el constructor cargar los atributos y luego en otro
+        //método imprimir sus datos y por último uno que imprima un mensaje si debe
+        //pagar impuestos(si el sueldo supera a 3000)
+        private string nombre;
+        private int sueldo;
+
+        public Empleado() 
+        {
+            string linea;
+
+            Console.Write("nombre del empleado: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese el sueldo del empleado: ");
+            linea = Console.ReadLine();
+            sueldo=int.Parse(linea);
+        }
+
+        public void Imprimir() 
+        {
+            Console.WriteLine("Nombre del empleado: " + nombre);
+            Console.WriteLine("Sueldo del empleado: " + sueldo);
+        }
+
+        public void Impuestos() 
+        {
+            if (sueldo > 3000) 
+            {
+                Console.WriteLine("Debe pagar impuestos");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Empleado emp = new Empleado();
+            emp.Imprimir();
+            emp.Impuestos();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThiagoAnzaldo-Act13/Punto2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    //2. Implementar la clase operaciones.Se deben cargar dos valores enteros en el
+    //constructor, calcular su suma, resta, multiplicación y división, cada una en un
+    //método, imprimir dichos resultados.
+    class Operaciones
+    {
+        private int valor1, valor2, suma, resta , multiplicacion;
+        private double division;
+        public Operaciones() 
+        {
+            string linea;
+
+            Console.Write("Ingrese un valor: ");
+            linea = Console.ReadLine();
+            valor1 = int.Parse(linea);
+
+            Console.Write("ingrese el segundo valor");
+            linea = Console.ReadLine();
+            valor2 = int.Parse(linea);
+        }
+
+        public void Suma() 
+        {
+            suma = valor1 + valor2;
+        }
+
+        public void Resta()
+        {
+            resta = valor1 - valor2;
+        }
+        public void Multiplicacion()
+        {
+            multiplicacion = valor1 * valor2;
+        }
+        public void Division()
+        {
+            division = valor1 / valor2;
+        }
+
+        public void ImprimirResultados() 
+        {
+            Console.WriteLine("resultado de la suma es: " + suma);
+            Console.WriteLine("resultado de la resta es: " + resta);
+            Console.WriteLine("resultado de la multiplicacion es: " + multiplicacion);
+            Console.WriteLine("resultado de la division es: " + division);
+
+        }
+        static void Main(string[] args)
+        {
+            Operaciones op = new Operaciones();
+            op.Suma();
+            op.Resta();
+            op.Multiplicacion();
+            op.Division();
+            op.ImprimirResultados();
+            Console.ReadKey();
+        }
+    }
+}
