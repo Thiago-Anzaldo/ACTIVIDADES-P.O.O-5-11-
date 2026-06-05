aaaa5801be283b1a65eff90dae3a249d9c7ca187
Thiago Anzaldo <thiagoanzaldo008@gmail.com>
2026-06-05 12:02:39 -0300

    Add files via upload

---
+++ ThiagoAnzaldo-Act16/Punto1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    class Persona 
+    {
+        protected string nombre;
+        protected int edad;
+
+        public string Nombre 
+        {
+            set 
+            {
+                nombre = value;
+            }
+            get 
+            { 
+                return nombre; 
+            }
+        }
+        public int Edad
+        {
+            set
+            {
+                edad = value;
+            }
+            get
+            {
+                return edad;
+            }
+        }
+
+        public void muestraPersona()
+        {
+            Console.WriteLine("Nombre: "+nombre);
+            Console.WriteLine("edad: "+edad);
+
+        }
+    }
+    class Empleado :Persona
+    {
+        private int sueldo;
+
+        public int Sueldo 
+        {
+            set 
+            {
+                sueldo = value;
+            }
+            get
+            {
+                return sueldo;
+            }
+        }
+        public void muestraEmpleado()
+        {
+            Console.WriteLine("Nombre: "+ nombre);
+            Console.WriteLine("edad: "+ edad);
+            Console.WriteLine("sueldo: "+ sueldo);
+        }
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona();
+            p1.Nombre = "mohamed";
+            p1.Edad = 67;
+            p1.muestraPersona();
+            Console.WriteLine("\n");
+
+            Persona p2 = new Persona();
+            p2.Nombre = "rober";
+            p2.Edad = 71;
+            p2.muestraPersona();
+            Console.WriteLine("\n");
+
+            Empleado e = new Empleado();
+            e.Nombre = "benson";
+            e.Edad = 45;
+            e.Sueldo = 300;
+            e.muestraEmpleado();
+            Console.WriteLine("\n");
+
+            Empleado e2 = new Empleado();
+            e2.Nombre = "pepe";
+            e2.Edad = 15;
+            e2.Sueldo = 30;
+            e2.muestraEmpleado();
+            Console.WriteLine("\n");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThiagoAnzaldo-Act16/Punto2/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    //2. Plantear una clase Producto y otra clase Inventario.
+    //La clase Producto debe tener como atributos privados el nombre, precio y
+    //stock.Definir propiedades para acceder a estos atributos, asegurando que el
+    //stock no pueda ser negativo y el precio sea mayor a cero.
+    //La clase Inventario debe contener 3 objetos de la clase Producto.Definir un
+    //método para mostrar todos los productos ordenados de menor a mayor en
+    //base al precio, además, mostrar el producto más caro y más barato del
+    //inventario.
+
+    class Producto 
+    {
+        private string nombre;
+        private int stock;
+        private float precio;
+
+        public string Nombre 
+        {
+            set 
+            {
+                nombre = value;
+            }
+            get 
+            {
+                return nombre;
+            }
+        }
+        public int Stock 
+        {
+            set 
+            {
+                stock = value;
+            }
+            get 
+            {
+                return stock;
+            }
+        }
+        public float Precio
+        {
+            set
+            {
+                    precio = value;
+            }
+            get
+            {
+                return precio;
+            }
+        }
+    } 
+    class Inventario
+    {
+        private float[] productos;
+        private Producto p1, p2, p3;
+
+        public Inventario() 
+        {
+            p1 = new Producto();
+            p2 = new Producto();
+            p3 = new Producto();
+        }
+
+        public void IngresoDatos()
+        {
+
+            string linea;
+            Console.Write("Nombre del producto: ");
+            p1.Nombre=Console.ReadLine();
+            do
+            {
+                Console.WriteLine("ingrese el stock: ");
+                linea = Console.ReadLine();
+                p1.Stock = int.Parse(linea);
+            }
+            while (p1.Stock < 0);
+            do {
+                Console.WriteLine("ingrese el precio: ");
+                linea = Console.ReadLine();
+                p1.Precio = float.Parse(linea);
+            }
+            while (p1.Precio < 1);
+
+            Console.Write("Nombre del producto: ");
+            p2.Nombre = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("ingrese el stock: ");
+                linea = Console.ReadLine();
+                p2.Stock = int.Parse(linea);
+            }
+            while (p2.Stock < 0);
+            do
+            {
+                Console.WriteLine("ingrese el precio: ");
+                linea = Console.ReadLine();
+                p2.Precio = float.Parse(linea);
+            }
+            while (p2.Precio < 1);
+
+            Console.Write("Nombre del producto: ");
+            p3.Nombre = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("ingrese el stock: ");
+                linea = Console.ReadLine();
+                p3.Stock = int.Parse(linea);
+            }
+            while (p3.Stock < 0);
+            do
+            {
+                Console.WriteLine("ingrese el precio: ");
+                linea = Console.ReadLine();
+                p3.Precio = float.Parse(linea);
+            }
+            while (p3.Precio < 1);
+        }
+
+        //public void MayorAMenor() 
+        //{
+        //    invent[0] = p1.Precio;
+        //}
+        static void Main(string[] args)
+        {
+            Inventario inventario = new Inventario();
+            inventario.IngresoDatos();
+        }
+    }
+}
