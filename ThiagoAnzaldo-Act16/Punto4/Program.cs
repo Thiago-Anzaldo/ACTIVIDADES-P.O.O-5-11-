e0eae321cb43658ad8540d74d9ff12b2a116a9af
Thiago Anzaldo <thiagoanzaldo008@gmail.com>
2026-06-24 21:50:32 -0300

    Add files via upload

--- ThiagoAnzaldo-Act16/Punto4/Program.cs
+++ ThiagoAnzaldo-Act16/Punto4/Program.cs
@@ -1,65 +1,85 @@
-﻿    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Linq;
-    using System.Runtime.ConstrainedExecution;
-    using System.Security.Cryptography.X509Certificates;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
 
-    namespace Punto4
+namespace Punto4
+{
+    //4. Armar tres clases: Animal, Mamífero y Perro.
+    //La clase Animal debe tener un atributo especie.
+    //La clase Mamífero, que hereda de Animal, debe tener un atributo
+    //tipoAlimentacion.
+    //La clase Perro, que hereda de Mamífero, debe tener un atributo nombre.
+    //Cada clase debe tener un constructor que reciba los datos correspondientes
+    //y los imprima indicando a qué clase pertenecen.Los datos deben ser
+    //asignados previamente
+    //Crear un objeto de la clase Perro y verificar la ejecución en cadena de los
+    //constructores.
+
+    class Animal
     {
-        //4. Armar tres clases: Animal, Mamífero y Perro.
-        //La clase Animal debe tener un atributo especie.
-        //La clase Mamífero, que hereda de Animal, debe tener un atributo
-        //tipoAlimentacion.
-        //La clase Perro, que hereda de Mamífero, debe tener un atributo nombre.
-        //Cada clase debe tener un constructor que reciba los datos correspondientes
-        //y los imprima indicando a qué clase pertenecen.Los datos deben ser
-        //asignados previamente
-        //Crear un objeto de la clase Perro y verificar la ejecución en cadena de los
-        //constructores.
+        protected string especie;
 
-        class Animal
+        public string Especie
         {
-            protected string especie;
-
-            public Animal()
-            {
-                especie="mamifero";
-            Console.WriteLine(especie+" (clase animal)");
-            }
+            set { especie = value; }
+            get { return especie; }
         }
+        public Animal()
+        {
+            especie = "mamifero";
+            Console.WriteLine(especie + " (clase animal)");
+        }
+    }
 
 
-            class Mamifero : Animal
-            {
-            private string tipoAlimentacion;
+    class Mamifero : Animal
+    {
+        private string tipoAlimentacion;
 
-            public Mamifero()
-            {
-                tipoAlimentacion = "carnivoro";
+        public string TipoAlimentacion
+        {
+            set { tipoAlimentacion = value; }
+            get { return tipoAlimentacion; }
+        }
+        public Mamifero()
+        {
+            tipoAlimentacion = "carnivoro";
             Console.WriteLine(tipoAlimentacion + " (clase mamifero)");
         }
 
-            public class Perro : Mamifero
+        public class Perro : Mamifero
+        {
+
+            private string nombre;
+            public string Nombre
             {
-                private string nombre;
-                public Perro() 
-                {
-                    nombre = "preguntale";
+                set { nombre = value; }
+                get { return nombre; }
+            }
+            public Perro()
+            {
+                nombre = "preguntale";
                 Console.WriteLine(nombre + " (clase perro)");
             }
 
-            }
+        }
 
-            class Prueba
+        class Prueba
+        {
+            static void Main(string[] args)
             {
-                static void Main(string[] args)
-                {
-                    Perro p = new Perro();
-                    Console.ReadKey();
-                }
+
+                Perro p = new Perro();
+                p.Nombre = "juan";
+                p.TipoAlimentacion = "carnivoro";
+                p.Especie = "mamifero";
+                Console.ReadKey();
             }
         }
     }
+}
